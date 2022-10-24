ea024cbe3e2b7fec4875c4294d2e87af9dddaa68
Andrii Kolomiichenko <bboywilld@gmail.com>
2022-10-24 17:49:35 +0700

    Change builder

--- builder.sh
+++ builder.sh
@@ -1,13 +1,18 @@
+mkdir -p ./builds
+
 # Build binary for:
 
 # Linux
 GOOS=linux GOARCH=386 go build -o ./builds/app_linux_386
-GOOS=linux GOARCH=amd64 go build -o ./builds/app_linux_amd64
+GOOS=linux GOARCH=amd64 go build -o ./builds/app_linux_x64
 # MacOS X
-GOOS=darwin GOARCH=amd64 go build -o ./builds/app_osx
+GOOS=darwin GOARCH=amd64 go build -o ./builds/app_osx_x64
+GOOS=darwin GOARCH=arm64 go build -o ./builds/app_osx_arm
 # Windows
 GOOS=windows GOARCH=386 go build -o ./builds/app_win_386.exe
 GOOS=windows GOARCH=amd64 go build -o ./builds/app_win_x64.exe
+# Raspberry Pi
+GOOS=linux GOARCH=arm GOARM=7 go build -o ./builds/app_linux_rpi
 
 # Save commit info
 git log -1 > ./builds/commit.info
